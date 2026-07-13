801f4f52223cf445723803ca636834fcc3269f21
Roy Bipro <roybipro896@gmail.com>
2026-07-13 17:00:13 +0600

    learn calss and how to call method and attributes

--- oop/oops.py
+++ oop/oops.py
@@ -6,8 +6,24 @@
 
 
 #functional programming
-def add(a , b):
-    print(a+b)
+# def add(a , b):
+#     print(a+b)
     
-add(12, 13)
-add(100, 200)
+# add(12, 13)
+# add(100, 200)
+
+
+#object oriented programming
+
+
+class Car:
+    a = 12 # inside class variabe call attribute
+
+    def hello():    # functions inside class is call method
+        print("how are you")  
+        
+
+#We can  access attributes and methodes after accessing the class    
+print(Car.a) #accessing attributes 
+
+Car.hello() #accessing methodes
