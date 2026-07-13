fddd60377864619ef5ecd02518246260a465a2b4
Roy Bipro <roybipro896@gmail.com>
2026-07-13 16:04:10 +0600

    learn python functional programing

--- oop/oops.py
+++ oop/oops.py
@@ -1,4 +1,13 @@
-a = 12
-b = 13
+#imperative programming
+# a = 12
+# b = 13
 
-print(a + b)
+# print(a + b)
+
+
+#functional programming
+def add(a , b):
+    print(a+b)
+    
+add(12, 13)
+add(100, 200)
