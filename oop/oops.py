c9193b51ac7f58d398d1b3f41383593904a64b35
Roy Bipro <roybipro896@gmail.com>
2026-07-13 17:07:05 +0600

    learn Objects and creating objects

--- oop/oops.py
+++ oop/oops.py
@@ -16,14 +16,32 @@
 #object oriented programming
 
 
-class Car:
-    a = 12 # inside class variabe call attribute
+# class
+# class Car:
+#     a = 12 # inside class variabe call attribute
 
-    def hello():    # functions inside class is call method
-        print("how are you")  
+#     def hello():    # functions inside class is call method
+#         print("how are you")  
         
 
-#We can  access attributes and methodes after accessing the class    
-print(Car.a) #accessing attributes 
+# #We can  access attributes and methodes after accessing the class    
+# print(Car.a) #accessing attributes 
 
-Car.hello() #accessing methodes
+# Car.hello() #accessing methodes
+
+
+# objects
+class Bags :
+    name = "Roys"
+    
+    def details(self):
+        print("This is the company who create bags")
+        
+
+rebook = Bags()
+campus = Bags()
+
+print(rebook.name)
+print(campus.name)
+
+rebook.details()
