b10163093cb4543bb8459f5290ea6e32e940ac19
Roy Bipro <roybipro896@gmail.com>
2026-07-20 14:02:16 +0600

    learn function overloading

--- oop/polymorphism.py
+++ oop/polymorphism.py
@@ -17,34 +17,43 @@
 
 # Method overriding (we need inheritance)
 
-class Animal:
+# class Animal:
     
-    a = 12
-    def __init__(self,name):
-        self.name = name
+#     a = 12
+#     def __init__(self,name):
+#         self.name = name
         
-    def details(self):
-        print(f"Your Name is {self.name}")
+#     def details(self):
+#         print(f"Your Name is {self.name}")
         
         
-class Humans(Animal):
+# class Humans(Animal):
     
-    b =13
-    def __init__(self, name,id):
-        super().__init__(name)
-        self.id = id
+#     b =13
+#     def __init__(self, name,id):
+#         super().__init__(name)
+#         self.id = id
         
-    def details(self):
-        super().details()
-        print(f"Your Name is {self.name} your id is {self.id}")
+#     def details(self):
+#         super().details()
+#         print(f"Your Name is {self.name} your id is {self.id}")
         
 
-obj = Humans("Dube", 1)
-obj2 = Animal("Bipro")
+# obj = Humans("Dube", 1)
+# obj2 = Animal("Bipro")
 
-print(obj.a)
-obj2.details()
-obj.details()
+# print(obj.a)
+# obj2.details()
+# obj.details()
 
-#when we doing inheritance and parent and chaild class 
+# when we doing inheritance and parent and chaild class 
 #have same method name so the child class method wll override your parent class method
+
+
+##method overloading
+class Hello:
+    def speak(self,a):
+        print("Hello from Hello class")
+    
+    def speak(self,a,b):
+        print("Hello from Hello class with two arguments")
