fa529dcfab2279a3be0112ea0a44b7110ee80913
Roy Bipro <roybipro896@gmail.com>
2026-07-26 16:03:00 +0600

    learn decorators

---
+++ decorators.py
@@ -0,0 +1,14 @@
+
+def extragreeting(func):
+    def wrapper():
+        print("Hello from Bangladesh")
+        func()
+        
+        print("Thank You Visit Again")
+    return wrapper
+
+@extragreeting
+def greetings():
+    print("Good morning")
+    
+greetings()
