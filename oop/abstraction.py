518ff4bfbe1575f3126a79c8feaf772c1c47ade5
Roy Bipro <roybipro896@gmail.com>
2026-07-26 09:04:55 +0600

    learn abstraction

---
+++ oop/abstraction.py
@@ -0,0 +1,22 @@
+from abc import ABC , abstractmethod
+
+class enforce(ABC):
+    @abstractmethod
+    def enginestrat():
+        pass
+
+class bike(enforce):
+    def enginestrat():
+        pass
+
+class car(enforce):
+    def enginestrat():
+        pass
+
+class truck:
+    pass
+
+
+onj1 = bike()
+onj2 = car()
+onj3 = truck()
