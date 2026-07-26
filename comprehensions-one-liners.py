4acae1fc86d21fb1754049462e9d168ead5737a4
Roy Bipro <roybipro896@gmail.com>
2026-07-26 18:08:32 +0600

    learn Multilevel inheriatance

---
+++ comprehensions-one-liners.py
@@ -0,0 +1,33 @@
+
+a = 20
+
+# if a % 2 == 0 :
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+# print("Even Number") if a%2 == 0 else print("Odd Number") #turnary operator
+
+
+# a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+# b = [i for i in a if i%2 == 0]
+
+# print(b)
+
+
+# def check(a):
+#     if a % 2 == 0:
+#         print("Even Number")
+#     else:
+#         print("Odd Number")
+        
+        
+# check(20)
+
+# check = lambda x: print("Even number") if x% 2==0 else print("Odd Number")
+
+# check(13)
+
+addition = lambda a, b: a + b
+print(addition(10,20))
