04dd3013b1cd73c5125df665935c1de07d21543a
Roy Bipro <roybipro896@gmail.com>
2026-07-27 11:34:06 +0600

    learn filter

--- map-filter-zip.py
+++ map-filter-zip.py
@@ -1,15 +1,25 @@
-# a = ["Bipro","Dipto","Rifat","Sakib"]
+# # a = ["Bipro","Dipto","Rifat","Sakib"]
 
-# lengths = map(len, a)
-# print(list(lengths))
+# # lengths = map(len, a)
+# # print(list(lengths))
 
-temp_cel = [0, 10, 20, 34.5]
+# temp_cel = [0, 10, 20, 34.5]
 
-# def converter(a):
-#     farenheit =(9/5)*a + 32
-#     return farenheit
+# # def converter(a):
+# #     farenheit =(9/5)*a + 32
+# #     return farenheit
 
 
-print(list(map(lambda x : (9/5)*x + 32, temp_cel)))
+# print(list(map(lambda x : (9/5)*x + 32, temp_cel)))
 
-# print(list(map(converter, temp_cel)))
+# # print(list(map(converter, temp_cel)))
+
+
+
+#filter 
+
+math_marks = [90, 25, 67, 45, 80, 89, 99, 100, 34]
+
+passed = filter(lambda x : x >= 50, math_marks)
+
+print(list(passed))
